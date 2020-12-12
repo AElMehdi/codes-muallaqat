07705e9b61053ef10bdfb887a19e9081ec426c1d
AElMehdi <assirelmehdi@gmail.com>
2020-12-12 18:44:19 +0100

    Cover existing TripService code fully

--- trip-service/java/src/test/java/com/aelmehdi/tripservicekata/trip/TripServiceTest.java
+++ trip-service/java/src/test/java/com/aelmehdi/tripservicekata/trip/TripServiceTest.java
@@ -2,32 +2,70 @@ package com.aelmehdi.tripservicekata.trip;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import com.aelmehdi.tripservicekata.exception.UserNotLoggedInException;
 import com.aelmehdi.tripservicekata.user.User;
 
 public class TripServiceTest {
 
-   private User myUser = new User();
+   private User aUser = new User();
 
    @Test
-   void should_get_trip_by_user() {
-      TripService tripService = createTripService();
-      myUser.addFriend(myUser);
-      myUser.addFriend(new User());
+   void should_raise_exception_when_not_logged() {
+      TripService tripService = createTripServiceNotLogged();
+      aUser.addFriend(aUser);
+      aUser.addFriend(new User());
 
-      List<Trip> tripsByUser = tripService.getTripsByUser(myUser);
+      assertThatThrownBy(() -> tripService.getTripsByUser(aUser))
+            .isInstanceOf(UserNotLoggedInException.class);
+   }
+
+
+   @Test
+   void should_get_trip_by_user_when_logged_and_has_friend() {
+      TripService tripService = createTripService();
+      aUser.addFriend(aUser);
+      aUser.addFriend(new User());
+
+      List<Trip> tripsByUser = tripService.getTripsByUser(aUser);
 
       assertThat(tripsByUser).isNotEmpty();
    }
 
+   @Test
+   void should_get_trip_by_user_when_logged_and_has_do_not_have_friends() {
+      TripService tripService = createTripService();
+      User userWithoutHimAsFriend = this.aUser;
+      userWithoutHimAsFriend.addFriend(new User());
+
+      List<Trip> tripsByUser = tripService.getTripsByUser(userWithoutHimAsFriend);
+
+      assertThat(tripsByUser).isEmpty();
+   }
+
+
    private TripService createTripService() {
       return new TripService() {
-
          @Override
          protected User getLoggedUser() {
-            return myUser;
+            return aUser;
+         }
+
+         @Override
+         protected List<Trip> findTripsByUser(User user) {
+            return asList(new Trip(), new Trip());
+         }
+      };
+   }
+
+   private TripService createTripServiceNotLogged() {
+      return new TripService() {
+         @Override
+         protected User getLoggedUser() {
+            return null;
          }
 
          @Override
